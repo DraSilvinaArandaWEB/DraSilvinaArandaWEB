90c474a98d19c915f7ac965c98b7b8c5f2a4d405
Francisco Meglia <megliafrancisco@gmail.com>
2024-11-27 23:47:05 -0300

    final update form

--- src/views/contacto/Contacto.jsx
+++ src/views/contacto/Contacto.jsx
@@ -1,4 +1,4 @@
-import React, { useRef , useState} from 'react'
+import React, { useRef, useState } from 'react'
 import './Contacto.css'
 import { Navbar } from '../../components/navbar/Navbar'
 import { Footer } from '../../components/footer/Footer'
@@ -27,60 +27,60 @@ export const Contacto = () => {
   //   }
   // }
 
-  // const submit = async (e) => {
-  //   e.preventDefault(); // Prevenir el comportamiento predeterminado
+  const submit = async (e) => {
+    e.preventDefault(); // Prevenir el comportamiento predeterminado
 
-  //   if (captcha.current.getValue()) {
-  //     console.log('CAPTCHA resuelto. Enviando formulario...');
-  //     setButtonText('ENVIANDO...'); // Cambiar el texto del botón
+    if (captcha.current.getValue()) {
+      console.log('CAPTCHA resuelto. Enviando formulario...');
+      setButtonText('ENVIANDO...'); // Cambiar el texto del botón
 
-  //     // Crear un objeto con los datos del formulario
-  //     const formData = new FormData(e.target);
+      // Crear un objeto con los datos del formulario
+      const formData = new FormData(e.target);
 
-  //     try {
-  //       const response = await fetch('https://formsubmit.co/frx.ros@gmail.com', {
-  //         method: 'POST',
-  //         body: formData,
-  //       });
+      try {
+        const response = await fetch('https://formsubmit.co/frx.ros@gmail.com', {
+          method: 'POST',
+          body: formData,
+        });
 
-  //       if (response.ok) {
-  //         alert('Formulario enviado exitosamente');
-  //         e.target.reset(); // Limpiar el formulario
-  //         captcha.current.reset(); // Reiniciar CAPTCHA
-  //       } else {
-  //         alert('Hubo un problema al enviar el formulario. Inténtalo nuevamente.');
-  //       }
-  //     } catch (error) {
-  //       console.error('Error al enviar el formulario:', error);
-  //       alert('Ocurrió un error. Por favor, inténtalo más tarde.');
-  //     } finally {
-  //       setButtonText('ENVIAR'); // Restablecer el texto del botón
-  //     }
-  //   } else {
-  //     console.log('CAPTCHA no resuelto. No se enviará el formulario.');
-  //     alert('Por favor, resuelve el CAPTCHA antes de enviar el formulario.');
-  //   }
-  // };
+        if (response.ok) {
+          alert('Formulario enviado exitosamente');
+          e.target.reset(); // Limpiar el formulario
+          captcha.current.reset(); // Reiniciar CAPTCHA
+        } else {
+          alert('Hubo un problema al enviar el formulario. Inténtalo nuevamente.');
+        }
+      } catch (error) {
+        console.error('Error al enviar el formulario:', error);
+        alert('Ocurrió un error. Por favor, inténtalo más tarde.');
+      } finally {
+        setButtonText('ENVIAR'); // Restablecer el texto del botón
+      }
+    } else {
+      console.log('CAPTCHA no resuelto. No se enviará el formulario.');
+      alert('Por favor, resuelve el CAPTCHA antes de enviar el formulario.');
+    }
+  };
 
   return (
     <div>
       <div className="navbar-container">
-      <Navbar></Navbar>
+        <Navbar></Navbar>
       </div>
       <Banner title={"CONTACTO"} subtitle={"Esperamos tu mensaje"}></Banner>
       <div className="contact-container">
         <div className="pink-line"></div>
-        <form action="https://formsubmit.co/frx.ros@gmail.com" method='POST'>
+        <form action="https://formsubmit.co/50368e4ee88e1166d3245b73f3331652" onSubmit={submit} method='POST'>
           <input type="hidden" name="_captcha" value="false"></input>
           <div className="container-1">
-            <input type="text" name='nombre' placeholder='Nombre' required/>
-            <input type="text" name='email' placeholder='Email' required/>
+            <input type="text" name='nombre' placeholder='Nombre' required />
+            <input type="text" name='email' placeholder='Email' required />
           </div>
           <div className="container-2">
-            <input type="text" placeholder='Telefono' required/>
+            <input type="text" placeholder='Telefono' required />
             <select name="asunto" id="asunto">
-            <option value="consulta">CONSULTA</option>
-            <option value="reserva">RESERVA</option>
+              <option value="consulta">CONSULTA</option>
+              <option value="reserva">RESERVA</option>
             </select>
           </div>
           <textarea name="mensaje" id="mensaje" placeholder='Mensaje...' required></textarea>
@@ -92,11 +92,11 @@ export const Contacto = () => {
                 onChange={onChange}
                 size='compact'
               /> : <ReCAPTCHA
-              ref={captcha}
-              sitekey="6Lc5RIwqAAAAAL4sppB4eTAELQaVlSxe6TF7z3b5"
-              onChange={onChange}
-              size='normal'
-            />}
+                ref={captcha}
+                sitekey="6Lc5RIwqAAAAAL4sppB4eTAELQaVlSxe6TF7z3b5"
+                onChange={onChange}
+                size='normal'
+              />}
             </div>
             <button>{buttonText}</button>
           </div>
