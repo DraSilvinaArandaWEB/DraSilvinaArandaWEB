d1f896099a34077621497667efc060b90e209f1a
DraSilvinaArandaWEB <drasilvinaarandaweb@gmail.com>
2026-01-08 14:20:29 -0300

    Update Contacto3.jsx

--- src/views/contact/Contacto.jsx
+++ src/views/contact/Contacto.jsx
@@ -115,12 +115,12 @@ export const Contacto = () => {
             <div className="recaptcha">
               {window.innerWidth < 600 ? <ReCAPTCHA
                 ref={captcha}
-                sitekey="6LcBO0QsAAAAAG0IZd1a7fhqLlahDTEQpJd2giIx"
+                sitekey={import.meta.env.VITE_RECAPTCHA_SITE_KEY}
                 onChange={onChange}
                 size='compact'
               /> : <ReCAPTCHA
                 ref={captcha}
-                sitekey="6LcBO0QsAAAAAG0IZd1a7fhqLlahDTEQpJd2giIx"
+                sitekey={import.meta.env.VITE_RECAPTCHA_SITE_KEY}
                 onChange={onChange}
                 size='normal'
               />}
